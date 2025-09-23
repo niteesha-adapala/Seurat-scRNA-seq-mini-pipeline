25a4f9b29141ef8e593d4f0034aaab4d3e3fbbb6
Niteesha Adapala <niteesha@udel.edu>
2025-09-23 14:17:04 -0400

    Save local changes (images in output/)

--- SingleCell_Mouse_AL_RNAseq.R
+++ SingleCell_Mouse_AL_RNAseq.R
@@ -5,11 +5,14 @@ library(Seurat)
 library(tidyverse)
 library(ggplot2)
 
+nsclc.sparse.m <- Read10X_h5(filename = "C:\Users/Niteesha/Desktop/mini-pro/20k_NSCLC_DTC_3p_nextgem_intron_donor_1_count_sample_feature_bc_matrix.h5")
+
 ## Load the dataset
 
 mats <- Seurat::Read10X_h5(
   "C:/Users/Niteesha/Downloads/Multiome_RNA_ATAC_Mouse_Brain_Alzheimers_AppNote_filtered_feature_bc_matrix.h5"
 )
+nsclc.sparse.m -> mats
 obj <- CreateSeuratObject(counts = if (is.list(mats)) mats[[1]] else mats)
 mats -> mouseAL
 
@@ -83,11 +86,11 @@ mouseAL.obj <- FindNeighbors(mouseAL.obj, dims = 1:15)
 mouseAL.obj <- FindClusters(mouseAL.obj, resolution = c(0.1,0.3, 0.5, 0.7, 1))
 View(mouseAL.obj@meta.data)
 
-DimPlot(mouseAL.obj, group.by = "RNA_snn_res.0.3", label = TRUE)
+DimPlot(mouseAL.obj, group.by = "RNA_snn_res.0.1", label = TRUE)
 
 # setting identity of clusters
 Idents(mouseAL.obj)
-Idents(mouseAL.obj) <- "RNA_snn_res.0.3"
+Idents(mouseAL.obj) <- "RNA_snn_res.0.1"
 Idents(mouseAL.obj)
 
 # non-linear dimensionality reduction
@@ -98,4 +101,49 @@ mouseAL.obj <- RunUMAP(mouseAL.obj, dims = 1:15)
 DimPlot(mouseAL.obj, reduction = "umap")
 
 # Biomarkers:
-cluster2.markers <- FindMarkers(pbmc, ident.1 = 2)
+#cluster2.markers <- FindMarkers(pbmc, ident.1 = 2)
+
+
+# find markers for all clusters
+all.markers <- FindAllMarkers(
+  mouseAL.obj, 
+  only.pos = TRUE,   # only positive markers (upregulated in cluster)
+  min.pct = 0.25,    # expressed in at least 25% of cells in cluster
+  logfc.threshold = 0.25
+)
+
+# view top markers per cluster
+head(all.markers)
+
+
+top10 <- all.markers %>% 
+  group_by(cluster) %>% 
+  top_n(n = 10, wt = avg_log2FC)
+
+# heatmap of top 10 markers per cluster
+DoHeatmap(mouseAL.obj, features = top10$gene) + NoLegend()
+
+#Step4
+new.cluster.ids <- c("Astrocytes", "Neurons", "Microglia", "Oligodendrocytes")
+
+names(new.cluster.ids) <- levels(mouseAL.obj)
+mouseAL.obj <- RenameIdents(mouseAL.obj, new.cluster.ids)
+
+# plot again with labels
+DimPlot(mouseAL.obj, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+
+#Step 5
+install.packages('SingleR')
+library(SingleR)
+install.packages('celldex')
+library(celldex)
+
+# use mouse RNA reference
+ref <- MouseRNAseqData()
+pred <- SingleR(test = GetAssayData(mouseAL.obj, slot = "data"), ref = ref, labels = ref$label.main)
+
+# add predictions to metadata
+mouseAL.obj$SingleR <- pred$labels
+
+DimPlot(mouseAL.obj, group.by = "SingleR", label = TRUE)
+
